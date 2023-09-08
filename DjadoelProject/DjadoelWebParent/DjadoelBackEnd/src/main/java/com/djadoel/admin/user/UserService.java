0bf6d79dc72329848a08255a3396bf854f0fca7a
Ridho Suhaebi Arrowi <ridhorezi1212@gmail.com>
2023-09-08 16:23:30 +0700

    implement spring security user password encode

--- DjadoelProject/DjadoelWebParent/DjadoelBackEnd/src/main/java/com/djadoel/admin/user/UserService.java
+++ DjadoelProject/DjadoelWebParent/DjadoelBackEnd/src/main/java/com/djadoel/admin/user/UserService.java
@@ -6,6 +6,7 @@ import com.djadoel.common.entity.Role;
 import com.djadoel.common.entity.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,16 +17,26 @@ public class UserService {
 
 	@Autowired
 	private RoleRepository roleRepo;
-	
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	public List<User> listAll() {
 		return (List<User>) userRepo.findAll();
 	}
-	
+
 	public List<Role> listRoles() {
 		return (List<Role>) roleRepo.findAll();
 	}
 
+	private void encodePassword(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+	}
+
 	public void save(User user) {
+		encodePassword(user);
 		userRepo.save(user);
 	}
+
 }
