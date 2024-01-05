2168c2483621012ba1c100a653ddfcd93a2999f0
Ridho Suhaebi Arrowi <ridhorezi1212@gmail.com>
2024-01-05 16:48:04 +0700

    Setup package brand, code brand entity & repository, implement unit testing for brand repository

---
+++ DjadoelProject/DjadoelCommon/src/main/java/com/djadoel/common/entity/Brand.java
@@ -0,0 +1,94 @@
+package com.djadoel.common.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+/* 
+ * Created by: Ridho Suhaebi Arrowi
+ * IDE: Spring Tool Suite 4
+ * Information: ridhosuhaebi01@gmail.com
+*/
+
+@Entity
+@Table(name = "Brands")
+public class Brand {
+
+	// Initialized entity and column
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false, length = 45, unique = true)
+	private String name;
+
+	@Column(nullable = false, length = 128)
+	private String logo;
+
+	@ManyToMany
+	@JoinTable(name = "brands_categories", joinColumns = @JoinColumn(name = "brand_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
+
+	// Implement Constructor
+
+	public Brand() {
+
+	}
+
+	public Brand(String name) {
+		this.name = name;
+		this.logo = "default.png";
+	}
+
+	// Implement Getter & Setter
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLogo() {
+		return logo;
+	}
+
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
+	}
+
+	// implement to string
+
+	@Override
+	public String toString() {
+		return "Brand [id=" + id + ", name=" + name + ", categories=" + categories + "]";
+	}
+
+}
